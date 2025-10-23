d85e31627724a732116259ae58445eb9c0f003da
Maksim Ploter <maksim.ploter@gmail.com>
2025-10-23 21:51:00 +0300

    Fix BatchSampler

--- dataset/__init__.py
+++ dataset/__init__.py
@@ -2,38 +2,40 @@ from torchvision import datasets, transforms
 from dataset.episode_sampler import EpisodeSampler
 from torch.utils.data import RandomSampler, BatchSampler
 
+
 def build_dataset(args):
-    """Build MNIST dataset for training"""
-    transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.1307,), (0.3081,))
-    ])
+  """Build MNIST dataset for training"""
+  transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.1307,), (0.3081,))
+  ])
 
-    dataset = datasets.MNIST(
-        root='./',
-        train=True,
-        download=True,
-        transform=transform
-    )
+  dataset = datasets.MNIST(
+    root='./',
+    train=True,
+    download=True,
+    transform=transform
+  )
 
-    return dataset
+  return dataset
 
 
 def build_sampler(args, dataset):
-    if args.model == 'autoencoder':
-        sampler = RandomSampler(dataset)
-        sampler = BatchSampler(
-            sampler,
-            batch_size=args.batch_size,
-        )
-    else:
-      samples_per_class = args.k_shot + args.q_query
+  if args.model == 'autoencoder':
+    sampler = RandomSampler(dataset)
+    sampler = BatchSampler(
+      sampler,
+      batch_size=args.batch_size,
+      drop_last=False
+    )
+  else:
+    samples_per_class = args.k_shot + args.q_query
 
-      sampler = EpisodeSampler(
-          labels=dataset.targets,
-          classes_per_iteration=args.n_way,
-          samples_per_class=samples_per_class,
-          number_of_iterations=args.num_episodes
-      )
+    sampler = EpisodeSampler(
+      labels=dataset.targets,
+      classes_per_iteration=args.n_way,
+      samples_per_class=samples_per_class,
+      number_of_iterations=args.num_episodes
+    )
 
-    return sampler
+  return sampler
