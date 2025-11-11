42810315f21bb456dc4377caab02a7eb7751682b
Maksim Ploter <maksim.ploter@gmail.com>
2025-11-11 21:02:57 +0200

    Add helper functions

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,6 +2,18 @@
 Utility functions for the prototypical network project.
 """
 
+from utils.load_model import (
+  load_trained_model,
+  load_dataset_from_args,
+  load_model_and_dataset,
+  create_args_from_checkpoint
+)
 from utils.misc import is_thresholded_dataset
 
-__all__ = ['is_thresholded_dataset']
+__all__ = [
+  'is_thresholded_dataset',
+  'load_trained_model',
+  'load_dataset_from_args',
+  'load_model_and_dataset',
+  'create_args_from_checkpoint'
+]
