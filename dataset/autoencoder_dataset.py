01cb426803213a423120e26e299084b20dfd766a
Maksim Ploter <maksim.ploter@gmail.com>
2025-11-27 23:05:18 +0200

    Correct types

--- dataset/autoencoder_dataset.py
+++ dataset/autoencoder_dataset.py
@@ -4,13 +4,17 @@ from torch.utils.data import Dataset
 class AutoencoderDataset(Dataset):
   """
   Wrapper dataset for autoencoder training.
-  Returns flattened images as both input and target for reconstruction.
+  Returns flattened data as both input and target for reconstruction.
+
+  For different loss types:
+  - MSE/BCE (MNIST, TMNIST): input=float, target=float
+  - CrossEntropy (Chess): input=float, target=int64
   """
 
   def __init__(self, base_dataset):
     """
     Args:
-        base_dataset: The base MNIST dataset
+        base_dataset: The base dataset (MNIST, TMNIST, Chess, etc.)
     """
     self.base_dataset = base_dataset
 
@@ -18,14 +22,7 @@ class AutoencoderDataset(Dataset):
     return len(self.base_dataset)
 
   def __getitem__(self, idx):
-    """
-    Returns:
-        tuple: (flattened_image, flattened_image) for reconstruction task
-    """
-    image, _ = self.base_dataset[idx]
-
-    # Flatten the image
-    flattened = image.view(-1)
-
-    # Return flattened image as both input and target
-    return flattened, flattened
+    data, _ = self.base_dataset[idx]
+    flattened = data.view(-1)
+    target_tensor = flattened.long()
+    return flattened, target_tensor
